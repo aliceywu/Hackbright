0759f989290878fa7b07220b6e92c176bc0e3bc6
Alice Wu <msalicewu38@gmail.com>
2016-07-26 20:50:01 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#def hello_print():
+	#return "hello"
+
+#print hello_print()
+
+#hello_return = "hello"
+
+def hello_return(person):
+	return "say name " + person 
+
+print hello_return("ostrich")
+
+partner="Stephanie"
+
+print hello_return(partner)
